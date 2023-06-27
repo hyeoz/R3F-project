065d63ce265e336f96325932242517f4ed923d35
Hyewon Lee <76167244+hyeoz@users.noreply.github.com>
2023-06-27 16:38:02 +0900

    3D model 예제

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,79 +1,39 @@
-import {
-  Cloud,
-  Environment,
-  OrbitControls,
-  Sky,
-  Stage,
-} from "@react-three/drei";
-import { useRef } from "react";
-import { Perf } from "r3f-perf";
 import * as THREE from "three";
-import { useControls } from "leva";
+import { Gltf, OrbitControls, useGLTF } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Model from "./Model";
 
 export default function Experience() {
-  const cubeRef = useRef();
-  const sphereRef = useRef();
+  // const model = useLoader(GLTFLoader, "./books_with_magnifier.glb");
+  // console.log(model);
 
-  // const { sunPosition } = useControls({
-  //   sunPosition: { value: [1, 2, 3] }, // 태양의 위치는 광원의 위치와 맞추는 것이 좋음
-  // });
-  const { intensity } = useControls({
-    intensity: { value: 1, min: 0, max: 12 }, // 태양의 위치는 광원의 위치와 맞추는 것이 좋음
-  });
+  const model = useGLTF("./books_with_magnifier.glb")
 
   return (
     <>
-      {/* <fog attach="fog" color="gray" near={1} far={10} /> */}
-      {/* <Sky sunPosition={sunPosition} />
-      <Cloud color="white" position={[-4, 5, -25]} speed={1} />
-      <Cloud color="white" position={[-4, 5, 0]} speed={1} />
-      <Cloud color="white" position={[-4, 5, 25]} speed={1} /> */}
-      <Environment
-        background
-        // files={[
-        //   "./posx.jpg",
-        //   "./negx.jpg",
-        //   "./posy.jpg",
-        //   "./negy.jpg",
-        //   "./posz.jpg",
-        //   "./negz.jpg",
-        // ]} // old school
-        // preset="sunset" // drei 가 제공하는 저해상도 hrdi
-        files="./hdri/lake_pier_4k.hdr" // new
-      />
       <Perf position="top-left" />
-      <OrbitControls enableDamping={false} />
-      {/* <ambientLight intensity={0.5} />
-      <directionalLight position={[1, 2, 3]} intensity={1.5} /> */}
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} castShadow shadow-normalBias={0.03} />
+      <OrbitControls makeDefault />
+      <group>
+        {/* <primitive object={model.scene} scale={10} position-y={-0.7} /> */}
+        {/* drei 방식을 사용하면 draco 압축 제공 */}
+        {/* <Gltf src="./books_with_magnifier.glb" scale={10} /> */}
 
-      <Stage
-        shadows={{ type: "contact", opacity: 0.2, blur: 3, color: "#ff0000" }}
-        environment="sunset"
-        preset="soft"
-        intensity={1.6}
-      >
-        <group>
-          <mesh ref={sphereRef} position-x={-2}>
-            <sphereGeometry />
-            <meshStandardMaterial color="#82E0AA" envMapIntensity={intensity} />
-          </mesh>
-          <mesh ref={cubeRef} position-x={2} scale={1.5}>
-            <boxGeometry />
-            <meshStandardMaterial
-              color="#F9E79F"
-              envMapIntensity={intensity} // 환경으로 인한 조명 세기
-            />
-          </mesh>
-          {/* <mesh
+        <Model scale={10} />
+
+        <mesh
+          receiveShadow
           position-y={-1}
           rotation-x={THREE.MathUtils.degToRad(-90)}
           scale={10}
         >
           <circleGeometry />
-          <meshStandardMaterial color="#5D6D7E" envMapIntensity={intensity} />
-        </mesh> */}
-        </group>
-      </Stage>
+          <meshStandardMaterial color="#5D6D7E" />
+        </mesh>
+      </group>
     </>
   );
 }
