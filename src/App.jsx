4fe5347e49a7fe3d80167f5a15e1ee29449efacf
Hyewon Lee <76167244+hyeoz@users.noreply.github.com>
2023-06-27 11:55:33 +0900

    useFrame, orbitcontrols, light

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
+import * as THREE from 'three'
 
 function App() {
   return (
-    <Canvas>
-      {/* <mesh>
-        <meshNormalMaterial attach="material" />
-        <boxGeometry args={[2, 2, 2]} attach="geometry" />
-      </mesh> */}
+    <Canvas
+      // orthographic // 원근감이 없는 카메라
+      // camera={{
+      //   // fov: 45, // 화각 (deg 사용)
+      //   zoom: 50,
+      //   near: 0.1,
+      //   far: 200,
+      //   position: [3, 2, 6],
+      // }}
+      /* renderer 설정 (특별한 상황이 아니라면 그대로 사용) */
+      // dpr={[1,2]} // 해상도
+      // gl={{
+      //   antialias: true,
+      //   toneMapping: THREE.LinearToneMapping,
+      //   outputEncoding: THREE.LinearEncoding // deprecated
+      // }}
+    >
       <Experience />
     </Canvas>
   );
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,22 +1,57 @@
+import { useFrame, extend, useThree } from "@react-three/fiber";
+import { useRef } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+extend({ OrbitControls: OrbitControls }); // 강제로 컴포넌트화 시킴
 
 export default function Experience() {
+  const three = useThree();
+  const cubeRef = useRef();
+
+  useFrame((state, delta) => {
+    // 매 프레임마다 인자로 받은 함수가 실행됨
+    // state : three 관련 객체 (scene, camera, renderer...)
+    // delta : 프레임 사이 경과 시간
+    cubeRef.current.rotation.x += 0.01;
+    cubeRef.current.rotation.y += 0.01;
+    cubeRef.current.rotation.z += 0.01;
+  });
+
+  useFrame((state) => {
+    const angle = state.clock.elapsedTime; // 경과된 시간을 각도로 표현
+    state.camera.position.x = Math.sin(angle) * 8;
+    state.camera.position.z = Math.cos(angle) * 8;
+    state.camera.lookAt(0, 0, 0); // 원점에 고정
+  });
+
   return (
     <>
+      {/* 형광등 */}
+      <ambientLight intensity={0.5} />
+      {/* 태양광 */}
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+
+      {/* R3F 컴포넌트는 소문자로 시작 (three.js class 와 충돌 가능성) */}
+      <orbitControls args={[three.camera, three.gl.domElement]} />
       <group>
         <mesh position-x={-2}>
           <sphereGeometry />
-          <meshBasicMaterial color="#82E0AA" />
+          {/* light 의 영향을 받지 않음 */}
+          {/* <meshBasicMaterial color="#82E0AA" /> */}
+          <meshStandardMaterial color="#82E0AA" />
         </mesh>
 
-        <mesh position-x={2} scale={1.5}>
+        {/* dom 에 접근하기 위해서 useRef 사용 */}
+        <mesh position-x={2} position-y={1} scale={1.5} ref={cubeRef}>
           <boxGeometry />
-          <meshBasicMaterial color="#F9E79F" />
+          {/* <meshBasicMaterial color="#F9E79F" /> */}
+          <meshStandardMaterial color="#F9E79F" />
         </mesh>
 
         <mesh
           position-y={-1}
-          rotation-x={THREE.MathUtils.degToRad(-90)}
+          rotation-x={THREE.MathUtils.degToRad(-90)} // raidan 으로 변환 필요
           scale={10}
         >
           <circleGeometry />
