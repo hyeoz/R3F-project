82d73afcd60b1babd50dc1710892f2c14a29bf91
Hyewon Lee <76167244+hyeoz@users.noreply.github.com>
2023-06-28 14:25:48 +0900

    physics - 2 (physics force)

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,8 @@ function App() {
 
   return (
     <Canvas
-      // orthographic // 원근감이 없는 카메라
-      // camera={{
-      //   // fov: 45, // 화각 (deg 사용)
-      //   zoom: 50,
-      //   near: 0.1,
-      //   far: 200,
-      //   position: [3, 2, 6],
-      // }}
-      /* renderer 설정 (특별한 상황이 아니라면 그대로 사용) */
-      // dpr={[1,2]} // 해상도
-      // gl={{
-      //   antialias: true,
-      //   toneMapping: THREE.LinearToneMapping,
-      //   outputEncoding: THREE.LinearEncoding // deprecated
-      // }}
-      // shadow 설정 필요
       shadows
-      // onCreated={onCreated}
     >
-      {/* <color args={['gray']} attach={"background"} /> */}
       <Experience />
     </Canvas>
   );
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,66 +1,126 @@
-import { OrbitControls } from "@react-three/drei";
-import { Perf } from "r3f-perf";
-import { CuboidCollider, Physics, RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { CylinderCollider, Physics, RigidBody } from "@react-three/rapier";
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
 
 export default function Experience() {
+  const cube = useRef();
+  const twister = useRef();
+  const [hitSound] = useState(() => new Audio("./hit.mp3"));
+
+  const model = useGLTF("./books.glb"); // 모델에 충돌체를 적용할 때는 적합한 충돌체의 종류를 설정하는 것이 중요
+
+  // useFrame((state, delta) => {
+  //   const time = state.clock.getElapsedTime(); // 시간에 대한 라디안 각도
+
+  //   // 회전
+  //   const euler = new THREE.Euler(0, time, 0); // 오일러 각도 (회전)
+  //   const quaternion = new THREE.Quaternion(); // 오일러 값을 사원수 (또는 해밀턴수, 복소수를 확장해 만든 수 체계) 로 변환
+  //   quaternion.setFromEuler(euler);
+  //   twister.current.setNextKinematicRotation(quaternion);
+  //   // 이동
+  //   const x = Math.cos(time) * 2; // -2 ~ 2
+  //   twister.current.setNextKinematicTranslation({ x, y: 0.8, z: 0 });
+  // });
+
+  const cubeJump = () => {
+    console.log(cube.current, "===> Rigid body ref");
+    cube.current.applyImpulse({ x: 0, y: 5, z: 0 }, true); // y축 방향으로 5만큼 이동하는 힘을 준다 / 2번재 인자를 이용해 sleep mode 에서 깨움
+    // cube.current.applyTorqueImpulse({ x: 0, y: -1, z: 0 }); // y축 방향으로 1만큼 회전하는 힘을 준다
+    // force 를 주고 몇 초간 기다리면 sleep mode 에 빠진다
+    if (cube.current.isSleeping()) {
+      // 방법 1
+      console.log("Im sleeping");
+      cube.current.wakeUp();
+    }
+  };
+
+  const onCollisionEvent = (event) => {
+    console.log(
+      `${event.target.rigidBodyObject.name} is crashed to ${event.other.rigidBodyObject.name}`
+    );
+
+    hitSound.currentTime = 0; // 0초부터 시작하도록
+    hitSound.volume = Math.random();
+    hitSound.play();
+  };
+
   return (
     <>
-      <Perf position="top-left" />
-
-      <OrbitControls enableDamping={false} />
-
+      <Perf />
       <ambientLight intensity={0.5} />
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
 
-      <Physics debug>
-        <RigidBody colliders="ball">
-          <mesh position={[0, 4, 0]}>
+      <OrbitControls makeDefault />
+
+      <Physics debug gravity={[0, -9.81, 0]}>
+        {/* <RigidBody
+          colliders="ball"
+          position={[-1.5, 2, 0]}
+          restitution={1}
+          name="sphere"
+        >
+          <mesh castShadow>
             <sphereGeometry />
             <meshStandardMaterial color="#82E0AA" />
           </mesh>
-        </RigidBody>
-        {/* <RigidBody colliders="trimesh"> */}
-        <RigidBody
-          colliders={false} // 사용자 지정 충돌체 사용
-          position={[1, 1, -0.25]}
-          rotation={[Math.PI / 3, 0, 0]}
-          scale={[2, 1, 1]}
+        </RigidBody> */}
+
+        {/* <RigidBody
+          position={[1.5, 2, 0]}
+          ref={cube}
+          // gravityScale={0.5} // 중력의 기본값은 [0, -9.81, 0]
+          // restitution={0.5} // 반발력의 기본값은 0, 부딪히는 메쉬 모두 1인 경우 멈추지 않고 튀김
+          // friction={0} // 마찰력의 기본값은 0.7
+          // mass={0.1} // 질량
+          name="box"
+          onCollisionEnter={onCollisionEvent}
+          // onSleep={() => console.log('SLEEPING')}
+          // onWake={() => console.log('WAKE UP')}
         >
-          {/* <mesh position={[2, 2, 0]} scale={1.5}>
+          <mesh castShadow onClick={cubeJump}>
             <boxGeometry />
             <meshStandardMaterial color="#F9E79F" />
-          </mesh> */}
-          {/* 여러 개의 메쉬도 하나의 강체로 인식 가능 */}
-          {/* <mesh position={[2, 2, -1.4]}>
-            <boxGeometry args={[3, 2, 1]} />
-            <meshStandardMaterial color="#f9e70f" />
           </mesh>
-          <mesh position={[2,2,0.4]}>
-            <boxGeometry args={[1, 0.5, 2]} />
-            <meshStandardMaterial color="#f9e70f" />
-          </mesh> */}
-          <CuboidCollider args={[1.5, 1.5, 0.5]} />
-          <CuboidCollider
-            args={[1, 0.1, 3]}
-            position={[0, 0, -2]}
-            // rotation={[Math.PI / 3,0, 0]}
-            // scale={[10, 1, 1]}
-          />
-          <mesh>
-            <torusGeometry args={[1, 0.5, 16, 32]} />
-            <meshStandardMaterial color="blue" />
+        </RigidBody> */}
+
+        {/* <RigidBody
+          type="kinematicPosition"
+          ref={twister}
+          position={[0, -0.5, 0]}
+          name="stick"
+        >
+          <mesh scale={[0.5, 0.5, 4]}>
+            <boxGeometry />
+            <meshStandardMaterial color="yellow" />
           </mesh>
-        </RigidBody>
-        <RigidBody type="fixed">
+        </RigidBody> */}
+
+        <RigidBody
+          type="fixed"
+          restitution={1}
+          friction={1}
+          mass={1}
+          name="floor"
+        >
           <mesh
-            position-y={-1}
+            receiveShadow
+            position-y={-1.25}
             rotation-x={THREE.MathUtils.degToRad(-90)}
             scale={10}
           >
-            <circleGeometry />
+            <planeGeometry />
             <meshStandardMaterial color="#5D6D7E" />
           </mesh>
+        </RigidBody>
+
+        <RigidBody colliders={"trimesh"} position={[0, 5, 0]}>
+          {/* <CylinderCollider args={[0.5, 1.25]} /> */}
+          <primitive object={model.scene} scale={5} position-y={-0.5}>
+            <axesHelper />
+          </primitive>
         </RigidBody>
       </Physics>
     </>
