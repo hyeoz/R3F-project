4f03c50fa48376d31cdb2d5d7e45b46b364f4d1d
Hyewon Lee <76167244+hyeoz@users.noreply.github.com>
2023-06-29 15:13:24 +0900

    캐릭터 이동, 카메라가 캐릭터 따라다니도록, 그림자

--- src/Character.jsx
+++ src/Character.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from "react";
+import { forwardRef, useCallback, useEffect, useRef } from "react";
 import * as THREE from "three";
 import { useControls } from "leva";
 import { useFrame } from "@react-three/fiber";
@@ -6,14 +6,44 @@ import { useGLTF, useAnimations, useKeyboardControls } from "@react-three/drei";
 import { RigidBody, CapsuleCollider } from "@react-three/rapier";
 
 const CHARACTER_HEIGHT = 1.79;
-const CAPSULE_RADIUS = 0.3;
+const CAPSULE_RADIUS = 0.6;
+const WALK_SPEED = 1;
+const RUN_SPEED = 2;
 
-export default function Character() {
+const getDirectionOffset = (keys) => {
+  let directionOffset = 0; // w
+  if (keys.forward) {
+    if (keys.leftward) {
+      directionOffset = Math.PI / 4; // w+a (45)
+    } else if (keys.rightward) {
+      directionOffset = -Math.PI / 4; // w+d (-45)
+    }
+  } else if (keys.backward) {
+    if (keys.leftward) {
+      directionOffset = Math.PI / 4 + Math.PI / 2; // s+a (135)
+    } else if (keys.rightward) {
+      directionOffset = -Math.PI / 4 - Math.PI / 2; // s+d (-135)
+    } else {
+      directionOffset = Math.PI; // s (180)
+    }
+  } else if (keys.leftward) {
+    directionOffset = Math.PI / 2; // a (90)
+  } else if (keys.rightward) {
+    directionOffset = -Math.PI / 2; // d (-90)
+  }
+  return directionOffset;
+};
+
+function Character({orbitControls}, ref) {
   const model = useGLTF("./Robot.glb");
   const [subKeys, getSubKeys] = useKeyboardControls();
 
   const playActionNameRef = useRef();
   const modelRef = useRef();
+  // const rigidRef = useRef();
+  const speedRef = useRef(0); // state 로 관리 안하는 이유 -> state 사용 시 리렌더링
+
+  const rigidRef = ref;
 
   const animations = useAnimations(model.animations, model.scene);
   // const { animationName } = useControls({
@@ -54,11 +84,14 @@ export default function Character() {
       if (keys.walk) {
         // 걷기 기보드 액션이 동시에 눌리면
         playAction("Walk");
+        speedRef.current = WALK_SPEED;
       } else {
         playAction("Run");
+        speedRef.current = RUN_SPEED;
       }
     } else {
       playAction("Idle");
+      speedRef.current = 0; // 멈춤
     }
 
     // 캐릭터를 카메라 방향으로 회전
@@ -76,14 +109,42 @@ export default function Character() {
     const rotateQuaternion = new THREE.Quaternion();
     rotateQuaternion.setFromAxisAngle(
       new THREE.Vector3(0, 1, 0),
-      angleCameraDirectionAxisY
-    );
+      angleCameraDirectionAxisY + getDirectionOffset(keys)
+    ); // x,y,z 세 개의 축으로 회전하게 되면 회전할 당시 축 하나가 고정되게 되는데, (짐벌락) quaternion 사용 시 모든 방향 동시에 회전하기 때문에 이 이슈를 해결할 수 있음 (하지만 180도 이상 한 번에 회전하지 못한다는 문제가 있음)
 
     // 두번째 인자(5도) 만큼 회전하다가 첫번째 인자만큼 회전했을 때 멈춤
     model?.quaternion.rotateTowards(
       rotateQuaternion,
       THREE.MathUtils.degToRad(5)
     );
+
+    // 방향벡터를 이용해 캐릭터 이동
+    const walkDirection = new THREE.Vector3();
+    camera.getWorldDirection(walkDirection);
+    walkDirection.y = 0; // 카메라가 하늘을 바라볼 때도 바닥에서만 이동할 수 있도록
+    walkDirection.normalize(); // 항상 크기가 1인 단위벡터여야 하기때문에
+    walkDirection.applyAxisAngle(
+      new THREE.Vector3(0, 1, 0),
+      getDirectionOffset(keys)
+    ); // a,s,d 를 눌러 카메라 방향과 다른 방향을 보게 될 때 보정
+
+    const deltaX = walkDirection.x * speedRef.current * delta;
+    const deltaZ = walkDirection.z * speedRef.current * delta;
+
+    const currentX = rigidRef.current.translation().x + deltaX; // position 아님
+    const currentY = rigidRef.current.translation().y;
+    const currentZ = rigidRef.current.translation().z + deltaZ;
+
+    rigidRef.current.setTranslation({ x: currentX, y: currentY, z: currentZ });
+
+    // 카메라가 항상 캐릭터를 바라보도록
+    camera.position.x += deltaX; // 이동하는 만큼 카메라 포지션도 이동
+    camera.position.z += deltaZ;
+
+    if (orbitControls) {
+      // camera.lookAt() 보다 ref 가 상위라서 ref 사용
+      orbitControls.current.target.set(currentX, currentY, currentZ);
+    }
   });
 
   useEffect(() => {
@@ -96,8 +157,12 @@ export default function Character() {
     });
   }, [model]);
 
+  useEffect(() => {
+    rigidRef.current.lockRotations(true) // 회전하는 경우, 물리법칙이 작용되는 RigidBody 에는 회전이 적용될 필요가 없기때문에 rotation 을 잠굼
+  }, [])
+
   return (
-    <RigidBody colliders={false} position={[0.1, 10, 0]}>
+    <RigidBody colliders={false} position={[0.1, 10, 0]} ref={rigidRef}>
       <CapsuleCollider
         args={[CHARACTER_HEIGHT / 2 - CAPSULE_RADIUS, CAPSULE_RADIUS]}
       />
@@ -112,3 +177,5 @@ export default function Character() {
     </RigidBody>
   );
 }
+
+export default forwardRef(Character)
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,15 +4,74 @@ import { Physics, RigidBody } from "@react-three/rapier";
 import { Perf } from "r3f-perf";
 
 import Character from "./Character";
+import { useEffect, useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+
+const SHADOW_CAMERA_SIZE = 20;
 
 export default function Experience() {
+  const orbitControlsRef = useRef();
+  const lightRef = useRef(); // 광원
+  const shadowCameraRef = useRef(); // 절두체 (그림자가 표현되는 범위)
+  const rigidRef = useRef();
+
+  const scene = useThree((state) => state.scene);
+
+  // 그림자를 생성하는 카메라도 캐릭터를 계속 따라다니도록* 복습 필요
+  useFrame(() => {
+    if (rigidRef.current) {
+      const trans = rigidRef.current.translation();
+      const currentX = trans.x;
+      const currentY = trans.y;
+      const currentZ = trans.z;
+
+      const characterPos = new THREE.Vector3(currentX, currentY, currentZ); // 캐릭터 위치
+      const lightReverseDirection = new THREE.Vector3(0, 1, 1).normalize(); // 캐릭터를 기준으로 빛이 내려오는 반대 방향벡터 구하기
+      const currentPos = lightReverseDirection
+        .multiplyScalar(8) // 캐릭터와 광원 사이의 거리만큼
+        .add(characterPos); // 캐릭터 포지션 더해주기
+
+      if (lightRef) {
+        lightRef.current.target.position.copy(characterPos); // 광원의 target = 캐릭터
+        lightRef.current.position.copy(currentPos); 
+      }
+    }
+  });
+
+  useEffect(() => {
+    shadowCameraRef.current = new THREE.CameraHelper(
+      lightRef.current.shadow.camera
+    );
+    scene.add(shadowCameraRef.current);
+    scene.add(lightRef.current.target);
+
+    return () => {
+      scene.remove(shadowCameraRef.current);
+      scene.remove(lightRef.current?.target);
+    };
+  }, [lightRef.current]);
+
   return (
     <>
       <Perf position="top-left" />
-      <OrbitControls makeDefault enablePan={false} />
+      <OrbitControls makeDefault enablePan={false} ref={orbitControlsRef} />
 
       {/* <ambientLight intensity={0.2} /> */}
-      <directionalLight position={[0, 1, 2]} intensity={1} castShadow />
+      <directionalLight
+        position={[0, 1, 1]}
+        intensity={1}
+        castShadow
+        ref={lightRef}
+        shadow-normalBias={0.1} // 그림자 확대 시 노이즈 보정
+        // 임시방안 -> 절두체 크기를 키운다
+        // shadow-mapSize={[1024 * 4, 1024 * 4]}
+        // shadow-camera-near={1}
+        // shadow-camera-far={25}
+        // shadow-camera-top={SHADOW_CAMERA_SIZE}
+        // shadow-camera-bottom={-SHADOW_CAMERA_SIZE}
+        // shadow-camera-right={SHADOW_CAMERA_SIZE}
+        // shadow-camera-left={-SHADOW_CAMERA_SIZE}
+      />
 
       <Environment preset="city" intensity={1} />
       <SoftShadows size={8} focus={0} samples={8} />
@@ -45,7 +104,7 @@ export default function Experience() {
             <meshStandardMaterial color="#5d6d72" />
           </mesh>
         </RigidBody>
-        <Character />
+        <Character orbitControls={orbitControlsRef} ref={rigidRef} />
       </Physics>
     </>
   );
